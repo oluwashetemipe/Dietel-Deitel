eaaa82043ee0f9e7f2ddf0e12807c0860093c5f5
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-05-13 23:37:04 +0100

    New

--- src/Chap5/ForLoop.java
+++ src/Chap5/ForLoop.java
@@ -2,15 +2,15 @@ package Chap5;
 
 public class ForLoop {
     public static void main(String[] args) {
-//        for (int i = 1; i <= 10; i++){
-//            System.out.println(i);
-//        }
-//        for (int i = 1; i <= 5; i++){
-//            for (int j = 1; j <= 10; j++){
-//                System.out.print('*');
-//            }
-//            System.out.println();
-//        }
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 10; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
         //initialize count
         //use for loop to increment count
         //for every count print 1 - 10 using for loop
