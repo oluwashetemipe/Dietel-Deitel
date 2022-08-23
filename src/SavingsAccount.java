1c2602cff35127de2e85211e48ff5cd5f11a869c
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-08-23 20:47:34 +0100

    init

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,21 +1,32 @@
 public class SavingsAccount {
     private static int annualInterestRate;
-    private int savingsBalance;
+    private double savingsBalance;
 
-    public int getSavingsBalance() {
+    public double getSavingsBalance() {
         return savingsBalance;
     }
 
-    public void setSavingsBalance(int savingsBalance) {
+    public void setSavingsBalance(double savingsBalance) {
         this.savingsBalance = savingsBalance;
     }
-    //
 
+    public SavingsAccount(double savingsBalance) {
+        this.savingsBalance = savingsBalance;
+    }
 
-    public int calculateMonthlyInterest(){
+    public double calculateMonthlyInterest(){
         savingsBalance += savingsBalance * (annualInterestRate) / 12;
         return savingsBalance;
     }
+
+    public static int getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public static void setAnnualInterestRate(int annualInterestRate) {
+        SavingsAccount.annualInterestRate = annualInterestRate;
+    }
+
     public static void modifyInterestRate(int rate){
         annualInterestRate = rate;
     }
