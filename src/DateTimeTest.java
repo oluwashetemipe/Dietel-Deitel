e68df347ad89282463b885b8922074dc7b93948c
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-08-26 00:02:39 +0100

    init

---
+++ src/DateTimeTest.java
@@ -0,0 +1,10 @@
+public class DateTimeTest {
+    public static void main(String[] args) {
+        DateTime dateTime = new DateTime(3,29,2000);
+
+        for (int i = 0; i < 7;i++){
+            dateTime.nextDay();
+            System.out.println(dateTime.toString());
+        }
+    }
+}
