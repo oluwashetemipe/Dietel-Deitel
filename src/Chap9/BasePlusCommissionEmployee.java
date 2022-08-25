8bcc55e2eed815929bf6d1f872e7e35160fbe1b1
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-08-25 23:54:32 +0100

    init

---
+++ src/Chap9/BasePlusCommissionEmployee.java
@@ -0,0 +1,35 @@
+package Chap9;
+
+public class BasePlusCommissionEmployee {
+    private CommissionEmployee commissionEmployee;
+    private double baseSalary;
+
+
+
+    public BasePlusCommissionEmployee( double commissionRate, double baseSalary){
+         if (baseSalary < 0.0)
+             throw new IllegalArgumentException(
+                 "Base salary must be >= 0.0");
+
+         this.baseSalary = baseSalary;
+         }
+
+
+         public void setBaseSalary(double baseSalary) {
+         if (baseSalary < 0.0)
+             throw new IllegalArgumentException(
+                 "Base salary must be >= 0.0");
+
+         this.baseSalary = baseSalary;
+         }
+
+         public double getBaseSalary(){
+         return baseSalary;
+         }
+    public double earnings() {
+        return getBaseSalary() * commissionEmployee.getGrossSales() ;
+
+    }
+
+}
+//Ex 9.4
