40ca2de2317cb806e7327d6848adf1b1a27685eb
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-08-28 02:13:23 +0100

    update

--- src/Chap9/BasePlusCommissionEmployee.java
+++ src/Chap9/BasePlusCommissionEmployee.java
@@ -1,13 +1,14 @@
 package Chap9;
 
 public class BasePlusCommissionEmployee {
-    private CommissionEmployee commissionEmployee;
+    private final CommissionEmployee commissionEmployee;
     private double baseSalary;
 
 
 
-    public BasePlusCommissionEmployee( double commissionRate, double baseSalary){
-         if (baseSalary < 0.0)
+    public BasePlusCommissionEmployee( CommissionEmployee commissionEmployee,double commissionRate, double baseSalary){
+        this.commissionEmployee = commissionEmployee;
+        if (baseSalary < 0.0)
              throw new IllegalArgumentException(
                  "Base salary must be >= 0.0");
 
@@ -30,6 +31,11 @@ public class BasePlusCommissionEmployee {
          public double earnings() {
             return getBaseSalary() * commissionEmployee.earnings() ;
         }
+        @Override
+        public String toString()
+        {
+         return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f", "Base Commission employee",commissionEmployee.getFirstName() ,commissionEmployee.getLastName() , "social security number",commissionEmployee.getSsn() , "gross sales",commissionEmployee.getGrossSales() , "commission rate",commissionEmployee.getCommissionRate(), "Earning", earnings() );
+         }
 
 }
 //Ex 9.4
--- src/Chap9/CommissionEmployee.java
+++ src/Chap9/CommissionEmployee.java
@@ -1,43 +1,41 @@
 package Chap9;
 
-public class CommissionEmployee {
-    private final String firstName;
-    private final String lastName;
-    private final String socialSecurityNumber;
+public class CommissionEmployee extends Employee{
+//    private final String firstName;
+//    private final String lastName;
+//    private final String socialSecurityNumber;
     private double grossSales;
     private double commissionRate;
 
-    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
+    public CommissionEmployee(String firstName, String lastName, String ssn, double grossSales, double commissionRate) {
+        super(firstName,lastName,ssn);
         if (grossSales < 0.0)
             throw new IllegalArgumentException("Gross sales must be >= 0.0");
 
         if (commissionRate <= 0.0 || commissionRate >= 1.0)
             throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.socialSecurityNumber = socialSecurityNumber;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.socialSecurityNumber = socialSecurityNumber;
         this.grossSales = grossSales;
         this.commissionRate = commissionRate;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public String getSocialSecurityNumber() {
+//        return socialSecurityNumber;
+//    }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getSocialSecurityNumber() {
-        return socialSecurityNumber;
-    }
 
 
-  
 
-  
-
-  
     public double earnings(){
          return getCommissionRate() * getGrossSales();
     }
@@ -59,6 +57,10 @@ public class CommissionEmployee {
         if (commissionRate <= 0.0 || commissionRate >= 1.0)
             throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
         this.commissionRate = commissionRate;
+    }
 
+    @Override
+    public String toString() {
+        return String.format("%s: %s%n%.2f","Commission Employee",super.toString(),earnings());
     }
 }
